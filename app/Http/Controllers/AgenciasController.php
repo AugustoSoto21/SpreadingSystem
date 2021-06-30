20f5e264a46cfa78fed536bad466035c12c3917c
ccharaco <jcesarchg9@gmail.com>
2021-06-30 11:39:35 -0400

    creacion de migraciones

---
+++ app/Http/Controllers/AgenciasController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Agencias;
+use Illuminate\Http\Request;
+
+class AgenciasController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Agencias  $agencias
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Agencias $agencias)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Agencias  $agencias
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Agencias $agencias)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Agencias  $agencias
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Agencias $agencias)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Agencias  $agencias
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Agencias $agencias)
+    {
+        //
+    }
+}
