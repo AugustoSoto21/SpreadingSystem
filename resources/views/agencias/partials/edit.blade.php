c522c15248f6252e3cfe48cabc22fb54f970a146
antonileon <tonijleon@gmail.com>
2021-07-13 12:12:17 -0400

    edit de agencias con ajax

--- resources/views/agencias/partials/edit.blade.php
+++ resources/views/agencias/partials/edit.blade.php
@@ -7,9 +7,7 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form action="{{ route('agencias.update',1) }}" method="POST" data-parsley-validate>
-        @csrf
-        @method('PUT')
+      <form action="#" method="POST" data-parsley-validate>
         <div class="modal-body">
           <p align="center"><small>Todos los campos <b style="color: red;">*</b> son requeridos.</small></p>
           <input type="hidden" name="id_agencia" value="" id="id_agencia_edit" placeholder="">
@@ -19,16 +17,13 @@
                 <label for="agencia">Agencia <b style="color: red;">*</b></label>
                 <input type="text" name="agencia" id="agencia_edit" class="form-control" required="required" placeholder="Ingrese el nombre de la agencia" onkeyup="this.value = this.value.toUpperCase();">
               </div>
-              @error('agencia')
-                <div class="alert alert-danger">{{ $message }}</div>
-              @enderror
             </div>
           </div>
 
         </div>
         <div class="modal-footer justify-content-between">
           <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa fa-times"></i> Cerrar</button>
-          <button type="submit" class="btn btn-info"><i class="fa fa-save"></i> Guardar</button>
+          <button type="submit" class="btn btn-info" id="SubmitEditAgencia"><i class="fa fa-save"></i> Guardar</button>
         </div>
       </form>
     </div>
